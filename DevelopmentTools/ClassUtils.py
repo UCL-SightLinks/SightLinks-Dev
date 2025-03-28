d6a802308d1a17bac5a14922c6d80c2344b7d514
Edw00765 <141541468+Edw00765@users.noreply.github.com>
2025-03-28 10:30:54 +0000

    Update ClassUtils.py / commented ClassUtils.py

--- DevelopmentTools/ClassUtils.py
+++ DevelopmentTools/ClassUtils.py
@@ -9,9 +9,18 @@ import numpy as np
 from PIL import Image
 
 
-# The custom dataset object I used to load the segmented image dataset - will only fit our format of data!!!
+# The custom dataset object I used to load the segmented image dataset
 class CrosswalkDataset(Dataset):
+    """
+    A custom dataset class that is used to store and load the segmented image dataset used in training the 
+    classifiaction model.
+    """
     def __init__(self, src_dir, transform=None):
+        """
+        Args:
+            src_dir (string): The path to the source directory for the data stored by our model
+            transform (pytorch transform object): The pre-processing transform used before accessing any item in the dataset
+        """
         self.src_dir = src_dir
         self.transform = transform
 
@@ -20,9 +29,24 @@ class CrosswalkDataset(Dataset):
         self.label_paths = [file_path for file_path in dir_files if file_path.endswith(".txt")]
 
     def __len__(self):
+        """
+        Returns the length of the dataset, required for the instanciation of the object
+        """
         return len(self.image_paths)
     
     def __getitem__(self, index):
+        """
+        The access mechanism for any item in the dataset, loading an image from the source 
+        directory along with its associated label. Each individual access loads the image again.
+
+        Args:
+            index (int): The index of the image to be retrieved in alphanumerical ordering by name of file
+        
+        Returns:
+            (image, label):
+                image (depends on transform): A transformed version of the image stored in memory.
+                label (tensor): A binary class label for the retrieved image.
+        """
         image_path = os.path.join(self.src_dir, self.image_paths[index])
         label_path = os.path.join(self.src_dir, self.label_paths[index])
 
@@ -38,7 +62,6 @@ class CrosswalkDataset(Dataset):
         
         if self.transform is None:
             self.transform = transforms.ToTensor()
-            
         return (self.transform(image), torch.FloatTensor(label))
 
 
